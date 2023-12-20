255b53d7fd48f8864c79b66de3f362a89a7f6721
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2023-12-20 10:02:57 +0530

    buttons

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,28 +1,26 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css';
-import MainSection from './Components/body';
-import Navbar from './Components/Navbar';
-import Button from './Components/Button';
+import "./App.css";
+import MainSection from "./Components/body";
+import Navbar from "./Components/Navbar";
+import Button from "./Components/Button";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <div className="outer flex w-screen min-h-[100vh] flex-col sm:flex-row ">
-    <div className="second bg-indigo-700 w-[100%] h-[70vh] sm:w-[70%] sm:h-[100vh]">
-       <MainSection/>
-       <Navbar/>
-       </div>
-    <div className='first bg-sky-500 w-[100%] h-[30vh] sm:w-[30%] sm:h-[100vh]'>
-       
-       <Button/>
-       </div>
-       
-       </div>
+      <div className="outer flex w-screen min-h-[100vh] flex-col sm:flex-row ">
+        <div className="second bg-indigo-700 w-[100%] h-[70vh] sm:w-[70%] sm:h-[100vh] bg-[url('./assets/notes.jpg')] bg-no-repeat bg-center bg-cover">
+          <Navbar />
+          <MainSection />
+        </div>
+        <div className="first bg-sky-500 w-[100%] h-[30vh] sm:w-[30%] sm:h-[100vh] flex justify-center items-center bg-[#0E1428]">
+          <Button />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- main/src/Components/Button.jsx
+++ main/src/Components/Button.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function Button() {
   return (
-    <div>
-      buttonmmm
+    <div className='flex flex-row sm:flex-col justify-center items-center w-[100%] gap-6 flex-wrap'>
+      <button className="one text-[#F18805] text-lg p-2 border-2 border-solid border-[#F18805] rounded-xl w-[10rem]">Upper Case</button>
+      <button className="two text-[#F18805] text-lg p-2 border-2 border-solid border-[#F18805] rounded-xl w-[10rem]">Lower Case</button>
+      <button className="thee text-[#F18805] text-lg p-2 border-2 border-solid border-[#F18805] rounded-xl w-[10rem]">Capitalize</button>
+      <button className="four text-[#F18805] text-lg p-2 border-2 border-solid border-[#F18805] rounded-xl w-[10rem]">Remove spaces</button>
+
     </div>
   )
 }
